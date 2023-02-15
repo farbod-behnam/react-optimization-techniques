e264d6591e4f4cfc5c1ee6640addd60f954a82d8
farbod-behnam <farbod.behnam@gmail.com>
2023-02-15 17:19:32 +0330

    optimize toggleParagraphHandler with useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
 import Button from './components/UI/Button/Button';
 
 function App() {
   const [showParagraph, setShowParagraph] = useState<boolean>(false);
+  const [allowToggle, setAllowToggle] = useState<boolean>(false);
 
   console.log("APP RUNNING");
   
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraphState) => !prevShowParagraphState);
+  const toggleParagraphHandler = useCallback( () => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraphState) => !prevShowParagraphState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   }
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph</Button>
     </div>
   );
